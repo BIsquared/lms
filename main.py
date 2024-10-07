3e1c73a3ac572b364377396a7cb13c6cdec8fbd7
Zaseem-BIsquared <zaseem@bisquared.com>
2024-10-07 16:51:04 +0530

    Feature #1 - Upload XL with questions - Create upload form
    = Tested the form manually by uploading XLSX and other file types
    - Names of functions and variables are clear
    - Comments explain the non-obvious
    - Duplication - None

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ app, rt = fast_app(live=True)
 
 @rt('/')
 def get():
-    return Titled("Quiz")
+    return Titled("Quiz"), A("Upload Excel", href="/upload")
+
+@rt("/upload")
+def get():
+    group = Group(Input(type="file", name="file", required="true"), Button("Upload"))
+    form = Form(
+        group,
+        hx_post="/upload",
+        target_id="main",
+        enctype="multipart/form-data",  # multipart/form-data is required for file upload
+    )
+    return Title("LMS"), Container(form, Div(id="main"))
+
+@rt("/upload")
+def post(file: UploadFile): 
+    # TODO - File structure is assumed as of now. Hence we are NOT validating it
+    return "File uploaded successfully!"
 
 serve()
