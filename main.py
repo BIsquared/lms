ebe47ffdc72d1e286b0e4a4164503fd242dd35fb
Zaseem-BIsquared <zaseem@bisquared.com>
2024-10-08 16:30:03 +0530

    Feature #1 - Display the data from db as a table - Add question page
    - Tested by comparing the table with uploaded XL.
    - Names of functions and variables are clear
    - Comments explain the non-obvious
    - Duplication - None

--- main.py
+++ main.py
@@ -3,9 +3,32 @@ import pandas as pd
 from io import BytesIO
 
 
-app, rt, quizs, Quiz = fast_app(
+def render(quizzes):
+    return Tr(
+        Td(quizzes.question),
+        Td(quizzes.a),
+        Td(quizzes.b),
+        Td(quizzes.c),
+        Td(quizzes.d),
+        Td(quizzes.answers),
+        Td(quizzes.tag),
+    )
+
+
+custom_css = Style(
+    """
+    .freeze-header{
+    position: sticky;
+    top: 0;
+      }
+    """
+)
+
+
+app, route, quizzes, Quiz = fast_app(
     "data/quiz.db",
     live=True,
+    render=render,
     id=int,
     question=str,
     a=str,
@@ -15,21 +38,29 @@ app, rt, quizs, Quiz = fast_app(
     answers=str,
     tag=str,
     pk="id",
+    hdrs=[custom_css],
 )
 
+column_names = ["Question", "A", "B", "C", "D", "Answer", "Tag"]
 
-@rt("/")
+
+@route("/")
 def get():
-    return Titled("Quiz", A("Upload Excel", href="/upload"))
+    return Titled(
+        "Quiz",
+        A("Upload Excel", href="/upload"),
+        " | ",
+        A("All Questions", href="/questions"),
+    )
 
 
-@rt("/upload")
+@route("/upload")
 def get():
     group = Group(
         Input(
             type="file",
             name="file",
-            accept=".xlsx",  # This restict user to upload only excel file with .xlsx extension
+            accept=".xlsx",
             required="true",
         ),
         Button("Upload"),
@@ -43,35 +74,49 @@ def get():
     return Title("LMS"), Container(form, Div(id="main"))
 
 
-def standardize_columns(column_name):
-    cleaned_columns = column_name.strip().lower()
+def standardize_column(column_name):
+    cleaned_column = column_name.strip().lower()
     # Replace consecutive spaces with a single underscore
-    return re.sub(r"\s+", "_", cleaned_columns)
+    return re.sub(r"\s+", "_", cleaned_column)
 
 
 def convert_binary_to_df(file_content):
     # Create a BytesIO object to read it as excel
     excel_data = BytesIO(file_content)
     df = pd.read_excel(excel_data, dtype=object)
-    df.columns = [standardize_columns(col) for col in df.columns]
+    df.columns = [standardize_column(col) for col in df.columns]
     # If there is no answer then the default answer is 'A'
-    df.answers = df.answers.fillna("A") 
-    # Convert null values to empty strings for storing in DB
-    df = df.fillna("")
+    df.answers = df.answers.fillna("A")
     return df
 
 
-@rt("/upload")
+@route("/upload")
 async def post(file: UploadFile):
-    if not file.filename.endswith("xlsx"):  
+    if not file.filename.endswith("xlsx"):
         return "Invalid file type! - Only .xlsx files are allowed"
     file_content = await file.read()
     df = convert_binary_to_df(file_content)
+    # To easily insert all the data into db we need to convert it to list of dict
     data = df.to_dict(
         orient="records"  # This will give each row as a dict value with column name as key
     )
-    quizs.insert_all(data)
-    return P("Successfully added to DB")
+    # For testing purposes we are deleting all the data from db before inserting new data
+    quizzes.insert_all(data, truncate=True)
+    return (
+        P("Successfully added"),
+        # It will redirect to '/questions' after 1 sec
+        Meta(http_equiv="refresh", content="1; url=/questions"),
+    )
+
+
+@route("/questions")
+def get():
+    table = Table(
+        Thead(Tr(map(Th, column_names)), cls="freeze-header"),
+        Tbody(*quizzes()),
+        cls="striped",
+    )
+    return Container(table)
 
 
 serve()
