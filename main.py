25b8f20eab9f14b3217a948abaca0e3335f52765
Adhil <adhil@bisquared.com>
2024-10-22 14:37:11 +0530

    Merge branch 'issue-1-upload-excel-e2e' into main

--- main.py
+++ main.py
@@ -2,234 +2,156 @@ from fasthtml.common import *
 import openpyxl  # Additional Plugin for pandas to read Excel
 import pandas as pd
 from io import BytesIO
+import tempfile
+import xlwings as xw
 
 
-# Check if the answer is correct or not.
-def is_answer(answers, column_name):
-    return True if column_name in answers else False
-
-green_border = 'border: 1px solid #72BF78; padding: 10px;'
-
-def render(quizs):
-    quiz_id = f"q-{quizs.id}"
-    answers = quizs.answers
-    edit_link = A(
-        "✏️", hx_get=f"/edit/{quizs.id}", hx_replace_url="true", target_id="main"
-    )
-    delete_link = A("❌", hx_delete=f"/{quizs.id}", target_id=quiz_id, hx_swap="outerHTML")
+def render(quizzes):
     return Tr(
-        Td(edit_link, " | ", delete_link),
-        Td(quizs.question),
-        Td(P(quizs.a, style=f'{green_border if is_answer(answers, "A") else ""}')),
-        Td(P(quizs.b, style=f'{green_border if is_answer(answers, "B") else ""}')),
-        Td(P(quizs.c, style=f'{green_border if is_answer(answers, "C") else ""}')),
-        Td(P(quizs.d, style=f'{green_border if is_answer(answers, "D") else ""}')),
-        Td(quizs.answers),
-        Td(quizs.tag),
-        id=quiz_id,
+        Td(quizzes.question),
+        Td(quizzes.a),
+        Td(quizzes.b),
+        Td(quizzes.c),
+        Td(quizzes.d),
+        Td(quizzes.answers),
+        Td(quizzes.tag),
     )
 
 
-column_names = ["Manage", "Question", "A", "B", "C", "D", "Answers", "Tag"]
+custom_css = Style(
+    """
+    .freeze-header{
+    position: sticky;
+    top: 0;
+      }
+    """
+)
 
-custom_style =Style("""td > p{margin:0px;}""")
 
-
-app, rt, quizs, Quiz = fast_app(
+app, route, quizzes, Quiz = fast_app(
     "data/quiz.db",
     live=True,
     render=render,
     id=int,
-    tag=str,
     question=str,
     a=str,
     b=str,
     c=str,
     d=str,
     answers=str,
+    tag=str,
     pk="id",
-    hdrs=[custom_style]
+    hdrs=[custom_css],
 )
 
-
-question_redirect = RedirectResponse(
-    "/questions", status_code=303
-)  # To redirect to the questions page even if the method is not GET
+column_names = ["Question", "A", "B", "C", "D", "Answer", "Tag"]
 
 
-# Standardize the column names
-def clean_column_name(column_name):
-    # Strip leading and trailing whitespace
-    cleaned = column_name.strip()
+@route("/")
+def get():
+    return Titled(
+        "Quiz",
+        A("Upload Excel", href="/upload"),
+        " | ",
+        A("All Questions", href="/questions"),
+    )
+
+
+@route("/upload")
+def get():
+    group = Group(
+        Input(
+            type="file",
+            name="file",
+            accept=".xlsx",
+            required="true",
+        ),
+        Button("Upload"),
+    )
+    form = Form(
+        group,
+        hx_post="/upload",
+        target_id="main",
+        enctype="multipart/form-data",  # multipart/form-data is required for file upload
+    )
+    return Title("LMS"), Container(form, Div(id="main"))
+
+
+def standardize_column(column_name):
+    cleaned_column = column_name.strip().lower()
     # Replace consecutive spaces with a single underscore
-    cleaned = re.sub(r"\s+", "_", cleaned)
-    # Convert to lowercase
-    cleaned = cleaned.lower()
-    return cleaned
+    return re.sub(r"\s+", "_", cleaned_column)
 
 
-# Convert the binary to a pandas dataframe
 def convert_binary_to_df(file_content):
-    data = BytesIO(file_content)  # Reading the binary data in memory
-    df = pd.read_excel(data, dtype=object)
-    # Standardize the column names
-    df.columns = [clean_column_name(col) for col in df.columns]
+    # Create a BytesIO object to read it as excel
+    excel_data = BytesIO(file_content)
+    df = pd.read_excel(excel_data, dtype=object)
+    df.columns = [standardize_column(col) for col in df.columns]
+    # If there is no answer then the default answer is 'A'
     df.answers = df.answers.fillna("A")
-    # Convert the na values to empty strings to store in DB.
-    df = df.fillna("")
     return df
 
 
-# Uploading the data to the database
-def insert_data(df):
-    data = df.to_dict(orient="records")
-    quizs.insert_all(data, truncate=True)
-
-
-# Main page
-@rt("/")
-def get():
-    group = Group(Input(type="file", name="file", required="true"), Button("Upload"))
-    form = Form(
-        group,
-        id="upload-form",
-        hx_post="/upload",
-        target_id="main",
-        hx_replace_url="/questions",
-        enctype="multipart/form-data",  # multipart/form-data is required for file upload
-    )
-    all_ques_btn = Button(
-        "All Questions", hx_get="/questions", hx_replace_url="true", target_id="main"
-    )
-    return Container(form, all_ques_btn, id="main")
-
-
-# To upload the Excel file
-@rt("/upload")
+@route("/upload")
 async def post(file: UploadFile):
     if not file.filename.endswith("xlsx"):
-        return "Invalid file type!"
+        return "Invalid file type! - Only .xlsx files are allowed"
     file_content = await file.read()
     df = convert_binary_to_df(file_content)
-    insert_data(df)
-    return question_redirect
+    # To easily insert all the data into db we need to convert it to list of dict
+    data = df.to_dict(
+        orient="records"  # This will give each row as a dict value with column name as key
+    )
+    # For testing purposes we are deleting all the data from db before inserting new data
+    quizzes.insert_all(data, truncate=True)
+    return (
+        P("Successfully added"),
+        # It will redirect to '/questions' after 1 sec
+        Meta(http_equiv="refresh", content="1; url=/questions"),
+    )
 
 
-
-# Display all the questions as a table
-@rt("/questions")
+@route("/questions")
 def get():
-    table = (
-        Table(Thead(Tr(map(Th, column_names))), Tbody(*quizs()), cls="striped", style='border-collapse: separate;')
-        if quizs()
-        else P("No data")
+    table = Table(
+        Thead(Tr(map(Th, column_names)), cls="freeze-header"),
+        Tbody(*quizzes()),
+        cls="striped",
     )
-    upload_btn = Button("Upload", hx_get="/", hx_replace_url="true", target_id="main")
-    download_btn = A(Button("Export"), href="/download")
-    content = (table, upload_btn, " ", download_btn)
-    return Container(*content, id="main")
+    export_button = A(Button("Export"), href="/download")
+    return Container(table, export_button)
 
 
-# Delete a question from the database
-@rt("/{id}")
-def delete(id: int):
-    quizs.delete(id)
-
-
-# Edit Page for each question
-@rt("/edit/{id}")
-def get(id: int):
-    quiz = quizs.get(id)
-    header = Div(
-        Label("Tag", Input(type="text", id="tag", value=quiz.tag)),
-        Label("Question", Textarea(quiz.question, type="text", id="question")),
-        Hidden(id="id", value=quiz.id),
-    )
-    body = [
-        Group(
-            CheckboxX(is_answer(quiz.answers, "A"), id="A"), Input(id="a", value=quiz.a)
-        ),
-        Group(
-            CheckboxX(is_answer(quiz.answers, "B"), id="B"), Input(id="b", value=quiz.b)
-        ),
-        Group(
-            CheckboxX(is_answer(quiz.answers, "C"), id="C"), Input(id="c", value=quiz.c)
-        ),
-        Group(
-            CheckboxX(is_answer(quiz.answers, "D"), id="D"), Input(id="d", value=quiz.d)
-        ),
-    ]
-    footer = Div(
-        Button("Update", hx_post=f"/update#q-{id}", hx_replace_url="true", target_id="main"),
-        " ",
-        Button("Cancel", hx_get=f"/questions#q-{id}", hx_replace_url="true", target_id="main"),
-    )
-    return Container(
-        Form(
-            Card(*body, header=header, footer=footer, id="edit-form"),
-            hx_post=f"/update/{id}",
-        ),
-        id="main",
-    )
-
-
-# class to handle the checkboxes
-@dataclass
-class Options:
-    A: bool
-    B: bool
-    C: bool
-    D: bool
-
-
-# Update the question
-@rt("/update")
-def post(option: Options, quiz: Quiz):
-    answers = "".join(
-        letter for letter, value in option.__dict__.items() if value
-    )  # concating the correct options
-    if answers:
-        quiz.answers = answers
-    else:
-        quiz.answers = "A"
-    quizs.update(quiz)
-    return question_redirect
-
-
-# Handeling the Excel Export.
-@app.get("/download")
-async def download_excel():
-    # Create DataFrame and remove 'id' column
+@route("/download")
+def get():
+    df = pd.DataFrame(quizzes()).drop("id", axis=1).rename(columns=str.capitalize)
+    # Create a temporary directory to store the Excel file becasue xlwings doesn't support direct streaming
+    temp_dir = tempfile.mkdtemp()
+    temp_file_path = os.path.join(temp_dir, "quiz_data.xlsx")
     try:
-        df = pd.DataFrame(quizs()).drop("id", axis=1)
-        df.columns = df.columns.str.capitalize()
-    except KeyError:
-        return RedirectResponse("/")
-    # Create Excel file in memory
-    output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False)
-        # Get the workbook and the worksheet
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-        
-        # Create a table
-        worksheet.add_table(f'A1:{chr(65 + len(df.columns) - 1)}{len(df) + 1}',
-                            {'name': 'Quiz_table', 'columns': [{'header': col} for col in df.columns]})
+        with xw.Book() as workbook:
+            sheet = workbook.sheets.active
+            starting_cell = sheet.range("A1")
+            starting_cell.options(index=False).value = df
+            table_range = starting_cell.expand("table")
+            table = sheet.tables.add(table_range, name="Quiz_table")
+            table.range.column_width = 35
+            table.range.api.WrapText = True
 
-        # Apply text wrapping to the entire table
-        wrap_format = workbook.add_format({'text_wrap': True})
-        worksheet.set_column(0, len(df.columns) - 1, 32, wrap_format)
+            workbook.save(temp_file_path)
+        # For streaming we need to read the file content into a BytesIO object
+        with open(temp_file_path, "rb") as file_content:
+            output = BytesIO(file_content.read())
+    finally:
+        os.remove(temp_file_path)
+        os.rmdir(temp_dir)
 
-    # Prepare download headers
-    headers = {"Content-Disposition": 'attachment; filename="quiz_data.xlsx"'}
-    # Return Excel file as downloadable response
-    return Response(
-        output.getvalue(),
-        headers=headers,
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    )
-
+    headers = {
+        "Content-Disposition": "attachment; filename=quiz_data.xlsx",
+        "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",  # This is to tell the browser that this is an excel file
+    }
+    return StreamingResponse(output, headers=headers)
 
 
 serve()
--- test/main_test.py
+++ test/main_test.py
@@ -1,40 +1,26 @@
 from playwright.sync_api import Page, expect
-import pandas as pd
-import os
 
-upload_file_path = r"D:\Projects\lms\data\dummy_data.xlsx"    
+upload_file_path = r"D:\Projects\lms\data\powerquery_quiz.xlsx"
+
 
 def test_file_upload_and_display(page: Page) -> None:
     page.goto("http://localhost:5001/")
-    
+
+    page.get_by_role("link", name="Upload Excel").click()
     page.get_by_role("textbox").set_input_files(upload_file_path)
-
     page.get_by_role("button", name="Upload").click(timeout=2000)
-
     # to ensure the table is visible
     data_table = page.locator('table:has-text("")')
     expect(data_table).to_be_visible()
 
+
 def test_file_export_and_verify(page: Page) -> None:
     page.goto("http://localhost:5001/questions")
-
     # Download the file
     with page.expect_download() as download_info:
         page.get_by_role("button", name="Export").click()
     download = download_info.value
-    
+
     # Ensure the download is complete
     download_path = download.suggested_filename
-    download.save_as(download_path)
-
-    # Read and verify the downloaded file
-    try:
-        original_df = pd.read_excel(upload_file_path)
-        downloaded_df = pd.read_excel(download_path)
-        
-        # Compare the dataframes
-        assert original_df.equals(downloaded_df)
-    finally:
-        # Clean up: remove the downloaded file
-        if os.path.exists(download_path):
-            os.remove(download_path)
+    assert download_path.endswith(".xlsx")
